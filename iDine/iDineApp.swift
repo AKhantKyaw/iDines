c0ae4ff81b74a04a071f1fc57bee45c521df2997
AungKhantKyaw <103022859+AKhantKyaw@users.noreply.github.com>
2023-06-21 13:46:01 +0630

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Aung Khant Kyaw on 21/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
